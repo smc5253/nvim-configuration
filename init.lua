da9e59c8f795ff468f276741fa1d23e910a0d48b
Shabbir Chatrissa <shabbir.chatrissa.ap@gmail.com>
2025-10-14 19:47:56 +0530

    Unnecessary code removed

--- init.lua
+++ init.lua
@@ -1,6 +1,3 @@
-vim.cmd [[let &shell = '"C:\\Program Files\\Git\\bin\\bash.exe"']]
-vim.cmd [[let &shellcmdflag = '-s']]
-
 require("config.keymaps")
 require("config.options")
 require("config.lazy")
