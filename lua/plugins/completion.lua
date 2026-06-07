bc7ec538726e294d30a6c703f451da5e9ea831f3
Shabbir Chatrissa <shabbir.chatrissa.ap@gmail.com>
2026-06-07 12:17:13 +0000

    reverted completion changes due to bugs

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,55 +1,24 @@
 return {
-	"saghen/blink.cmp",
-	dependencies = { "rafamadriz/friendly-snippets" },
+  "saghen/blink.cmp",
+  dependencies = { "rafamadriz/friendly-snippets" },
 
-	version = "1.*",
+  version = "1.*",
 
-	opts = {
-		-- 1. Customizing the super-tab mapping to fix Emmet/Snippet navigation
-		keymap = {
-			preset = "none", -- Clear preset to prevent conflicts
+  opts = {
+    keymap = { preset = "super-tab" },
 
-			["<CR>"] = { "accept", "fallback" },
+    appearance = {
+      nerd_font_variant = "mono",
+    },
 
-			["<Tab>"] = {
-				function(cmp)
-					-- Prioritize jumping to the next Emmet placeholder/div first
-					if cmp.is_snippet_active() then
-						return cmp.snippet_forward()
-					elseif cmp.is_visible() then
-						return cmp.select_next()
-					end
-				end,
-				"fallback",
-			},
-			["<S-Tab>"] = {
-				function(cmp)
-					-- Prioritize jumping back a placeholder/div first
-					if cmp.is_snippet_active() then
-						return cmp.snippet_backward()
-					elseif cmp.is_visible() then
-						return cmp.select_prev()
-					end
-				end,
-				"fallback",
-			},
+    -- (Default) Only show the documentation popup when manually triggered
+    completion = { documentation = { auto_show = true } },
 
-			["<C-b>"] = { "scroll_documentation_up", "fallback" },
-			["<C-f>"] = { "scroll_documentation_down", "fallback" },
-			["<C-e>"] = { "hide", "fallback" },
-		},
+    sources = {
+      default = { "lsp", "path", "snippets", "buffer" },
+    },
 
-		appearance = {
-			nerd_font_variant = "mono",
-		},
-
-		completion = { documentation = { auto_show = true } },
-
-		sources = {
-			default = { "lsp", "path", "snippets", "buffer" },
-		},
-
-		fuzzy = { implementation = "prefer_rust_with_warning" },
-	},
-	opts_extend = { "sources.default" },
+    fuzzy = { implementation = "prefer_rust_with_warning" },
+  },
+  opts_extend = { "sources.default" },
 }
