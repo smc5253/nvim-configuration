a35e3658c54a2cf031e4132e5b20748a4b20b78d
Shabbir Chatrissa <shabbir.chatrissa.ap@gmail.com>
2026-06-07 10:52:49 +0000

    updated completions since preset=supbertab was messing with emmet jumping

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,24 +1,55 @@
 return {
-  "saghen/blink.cmp",
-  dependencies = { "rafamadriz/friendly-snippets" },
+	"saghen/blink.cmp",
+	dependencies = { "rafamadriz/friendly-snippets" },
 
-  version = "1.*",
+	version = "1.*",
 
-  opts = {
-    keymap = { preset = "super-tab" },
+	opts = {
+		-- 1. Customizing the super-tab mapping to fix Emmet/Snippet navigation
+		keymap = {
+			preset = "none", -- Clear preset to prevent conflicts
 
-    appearance = {
-      nerd_font_variant = "mono",
-    },
+			["<CR>"] = { "accept", "fallback" },
 
-    -- (Default) Only show the documentation popup when manually triggered
-    completion = { documentation = { auto_show = true } },
+			["<Tab>"] = {
+				function(cmp)
+					-- Prioritize jumping to the next Emmet placeholder/div first
+					if cmp.is_snippet_active() then
+						return cmp.snippet_forward()
+					elseif cmp.is_visible() then
+						return cmp.select_next()
+					end
+				end,
+				"fallback",
+			},
+			["<S-Tab>"] = {
+				function(cmp)
+					-- Prioritize jumping back a placeholder/div first
+					if cmp.is_snippet_active() then
+						return cmp.snippet_backward()
+					elseif cmp.is_visible() then
+						return cmp.select_prev()
+					end
+				end,
+				"fallback",
+			},
 
-    sources = {
-      default = { "lsp", "path", "snippets", "buffer" },
-    },
+			["<C-b>"] = { "scroll_documentation_up", "fallback" },
+			["<C-f>"] = { "scroll_documentation_down", "fallback" },
+			["<C-e>"] = { "hide", "fallback" },
+		},
 
-    fuzzy = { implementation = "prefer_rust_with_warning" },
-  },
-  opts_extend = { "sources.default" },
+		appearance = {
+			nerd_font_variant = "mono",
+		},
+
+		completion = { documentation = { auto_show = true } },
+
+		sources = {
+			default = { "lsp", "path", "snippets", "buffer" },
+		},
+
+		fuzzy = { implementation = "prefer_rust_with_warning" },
+	},
+	opts_extend = { "sources.default" },
 }
